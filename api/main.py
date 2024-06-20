f1ddfefc5781a06d3327ad4fa043508c617e7dcb
Andrew <andrew@andrewperlas.com>
2024-06-19 18:33:17 -1000

    importing modules into main.py

--- api/main.py
+++ api/main.py
@@ -1,1 +1,12 @@
 from flask import escape
+import firebase_admin
+
+from firebase_admin import credentials
+from firebase_admin import firestore
+from add_visit import current_count
+from add_visit import add_count
+
+cred = credentials.Certificate("sa-api-firestore.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
