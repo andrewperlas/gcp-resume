cd3c478572f87c617b98ca36aa0753cbea689d21
Andrew <andrew@andrewperlas.com>
2024-06-19 16:56:07 -1000

    separated POST and GET into their own python apps

---
+++ api/add_visit.py
@@ -0,0 +1,19 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("sa-api-firestore.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+# Find current visitor count
+def current_count():
+    count = db.collection('visits').count()
+    query_result = count.get()
+    total = query_result[0][0].value
+    return total
+
+# Add visits with auto IDs
+data = {'description': 'Another visitor count', 'visitorNumber': current_count()}
+db.collection('visits').add(data)
--- api/count_visits.py
+++ api/count_visits.py
@@ -7,10 +7,6 @@ firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-# Add visits with auto IDs
-data = {'description': 'Visit Count', 'name': 'Arnold'}
-#db.collection('visits').add(data)
-
 # Count visits
 count = db.collection('visits').count()
 query_result = count.get()
