f54d3c4d1f1cec66fca139d049ad784b1338b79c
Andrew <andrew@andrewperlas.com>
2024-06-19 16:38:32 -1000

    create function for counting number of visits

--- api/api.py
+++ api/api.py
@@ -1,5 +1,17 @@
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import firestore
 
 cred = credentials.Certificate("sa-api-firestore.json")
 firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+# Add visits with auto IDs
+data = {'description': 'Visit Count', 'name': 'Arnold'}
+#db.collection('visits').add(data)
+
+# Count visits
+count = db.collection('visits').count()
+query_result = count.get()
+print("Number of visitors:", query_result[0][0].value)
