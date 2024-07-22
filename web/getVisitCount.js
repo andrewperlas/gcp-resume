d1a53917440864b18c3cdb0fdd3378fc76623f1d
Andrew <andrew@andrewperlas.com>
2024-07-21 19:22:42 -1000

    update: cloudFunction endpoint and testing getVisitCount script

--- web/getVisitCount.js
+++ web/getVisitCount.js
@@ -1,10 +1,15 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     // The URL of our Cloud Function API
-    const cloudFunctionUrl = 'https://us-west1-psyched-age-416001.cloudfunctions.net/add-visitor-count'
+    const cloudFunctionUrl = 'https://us-west1-psyched-age-416001.cloudfunctions.net/api'
+
+    // Options for the fetch request
+    const postOptions = {
+        method: "GET"
+    }
 
     // GET request
-    fetch(cloudFunctionUrl)
+    fetch(cloudFunctionUrl, postOptions)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(json => p.innerHTML = json[0].description)
         .catch(error => console.error('Error:', error));
 });
