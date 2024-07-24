5387b1ea592fb62f8539967e709f525429ac360c
Andrew <andrew@andrewperlas.com>
2024-07-24 11:38:20 -1000

    GET should grab latest id value and not description

--- web/getVisitCount.js
+++ web/getVisitCount.js
@@ -10,6 +10,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // GET request
     fetch(cloudFunctionUrl, Options)
         .then(response => response.json())
-        .then(json => p.innerHTML = json[0].description)
+        .then(json => p.innerHTML = json[0].id)
         .catch(error => console.error('Error:', error));
 });
